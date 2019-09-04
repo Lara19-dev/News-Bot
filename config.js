a1d4823ce34e21715f8ff1d986bf436cd81acbe3
Senre <44935191+Senre@users.noreply.github.com>
2019-09-04 13:04:10 +0800

    added more configuration variables

--- config.js
+++ config.js
@@ -4,4 +4,6 @@ module.exports = {
     token: process.env.TOKEN,
     owner: process.env.OWNER,
     botVersion: process.env.BOTVERSION,
+    interval: process.env.INTERVAL,
+    prefix: process.env.PREFIX
 };
