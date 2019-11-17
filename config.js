ffd88eec3e32385a2c9c8fecb0209975a29e5f83
Senre <da.dr.senre@gmai.com>
2019-11-17 09:38:45 +0800

    Deleted unnecessary files

--- config.js
+++
@@ -1,9 +0,0 @@
-require('dotenv-flow').config();
-
-module.exports = {
-    token: process.env.TOKEN,
-    owner: process.env.OWNER,
-    botVersion: process.env.BOTVERSION,
-    interval: process.env.INTERVAL,
-    prefix: process.env.PREFIX
-};
